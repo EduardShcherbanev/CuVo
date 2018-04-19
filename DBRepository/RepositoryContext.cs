029a185796d61c6ce6c53e1a7f859a5c06cb6448
Eduard Shcherbanev <fireshock1@gmail.com>
2018-04-19 17:08:35 +0300

    Init

---
+++ DBRepository/RepositoryContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBRepository
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
+        {
+
+        }
+    }
+}
